c5dad05df0d4f2848f5539350d25713a4ab96517
fahmida-fahmi <abdulaziz21298@gmail.com>
2025-05-16 23:26:04 +0600

    fix the not found pages in render by creating a file named _redirect

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,26 @@ import react from "@vitejs/plugin-react";
 // import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     react({
-      jsxImportSource: 'react',
+      jsxImportSource: "react",
       babel: {
-        plugins: ['@babel/plugin-transform-react-jsx']
-      }
+        plugins: ["@babel/plugin-transform-react-jsx"],
+      },
     }),
     // tailwindcss(),
     // reactRouter(),
   ],
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
     include: /src\/.*\.jsx?$/,
     exclude: [],
   },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
     },
   },
